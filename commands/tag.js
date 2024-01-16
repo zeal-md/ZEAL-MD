8cece9511758fa086a5f2c200d7b8a9222b2f436
Zeal <152204049+zeal-md@users.noreply.github.com>
2024-01-16 11:55:38 +0100

    ZEAL-MD

--- commands/tag.js
+++ commands/tag.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2022 Sourav KL11.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
+ZEAL-MD - zeal
 */
 const {Module} = require('../main')
 const {getString} = require('./misc/lang');
